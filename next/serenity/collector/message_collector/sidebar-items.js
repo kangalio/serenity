addcbb4323568b69882c91b23ea3f7bc546bfaf9
kangalioo <kangalioo@users.noreply.github.com>
2022-09-11 16:55:42 +0000

    deploy: d1911a91cbcb74d588008da78c0692b98e3721fd

--- next/serenity/collector/message_collector/sidebar-items.js
+++ next/serenity/collector/message_collector/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["CollectReply",""],["MessageCollector","A message collector receives messages matching the given filter for a set duration."],["MessageCollectorBuilder","Future building a stream of messages."],["MessageFilter","Filters events on the shard’s end and sends them to the collector."]]});
+window.SIDEBAR_ITEMS = {"struct":[["FilterOptions",""]],"type":[["CollectReply",""],["MessageCollector",""],["MessageCollectorBuilder","A message collector receives messages matching the given filter for a set duration."],["MessageFilter",""]]};
