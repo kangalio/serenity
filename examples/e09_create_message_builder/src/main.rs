841ec7be3e977b2112abf7028b51bfbdec1fceab
kangalioo <jannik.a.schaper@web.de>
2022-09-19 07:47:59 +0200

    Remove e09 indentation

--- examples/e09_create_message_builder/src/main.rs
+++ examples/e09_create_message_builder/src/main.rs
@@ -12,34 +12,36 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            // The create message builder allows you to easily create embeds and messages
-            // using a builder syntax.
-            // This example will create a message that says "Hello, World!", with an embed that has
-            // a title, description, an image, three fields, and a footer.
-            let footer = CreateEmbedFooter::new("This is a footer");
-            let embed = CreateEmbed::new()
-                .title("This is a title")
-                .description("This is a description")
-                .image("attachment://ferris_eyes.png")
-                .fields(vec![
-                    ("This is the first field", "This is a field body", true),
-                    ("This is the second field", "Both fields are inline", true),
-                ])
-                .field("This is the third field", "This is not an inline field", false)
-                .footer(footer)
-                // Add a timestamp for the current time
-                // This also accepts a rfc3339 Timestamp
-                .timestamp(Timestamp::now());
-            let builder = CreateMessage::new()
-                .content("Hello, World!")
-                .embed(embed)
-                .add_file("./ferris_eyes.png");
-            let msg = msg.channel_id.send_message(&ctx.http, builder).await;
+        if msg.content != "!hello" {
+            return;
+        }
 
-            if let Err(why) = msg {
-                println!("Error sending message: {:?}", why);
-            }
+        // The create message builder allows you to easily create embeds and messages
+        // using a builder syntax.
+        // This example will create a message that says "Hello, World!", with an embed that has
+        // a title, description, an image, three fields, and a footer.
+        let footer = CreateEmbedFooter::new("This is a footer");
+        let embed = CreateEmbed::new()
+            .title("This is a title")
+            .description("This is a description")
+            .image("attachment://ferris_eyes.png")
+            .fields(vec![
+                ("This is the first field", "This is a field body", true),
+                ("This is the second field", "Both fields are inline", true),
+            ])
+            .field("This is the third field", "This is not an inline field", false)
+            .footer(footer)
+            // Add a timestamp for the current time
+            // This also accepts a rfc3339 Timestamp
+            .timestamp(Timestamp::now());
+        let builder = CreateMessage::new()
+            .content("Hello, World!")
+            .embed(embed)
+            .add_file("./ferris_eyes.png");
+        let msg = msg.channel_id.send_message(&ctx.http, builder).await;
+
+        if let Err(why) = msg {
+            println!("Error sending message: {:?}", why);
         }
     }
 
